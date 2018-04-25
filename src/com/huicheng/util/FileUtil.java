c2942730885a67214661e794b029cef1f93ae6cd
AaronDjc <aaron_djc@foxmail.com>
2018-04-25 16:32:31 +0800

    Create FileUtil.java
    文件上传工具类

---
+++ src/com/huicheng/util/FileUtil.java
@@ -0,0 +1,14 @@
+package com.huicheng.util;
+
+public class FileUtil {
+
+	private static final long serialVersionUID = 1L;
+    
+    // 上传文件存储目录
+    private static final String UPLOAD_DIRECTORY = "upload";
+ 
+    // 上传配置
+    private static final int MEMORY_THRESHOLD   = 1024 * 1024 * 3;  // 3MB
+    private static final int MAX_FILE_SIZE      = 1024 * 1024 * 40; // 40MB
+    private static final int MAX_REQUEST_SIZE   = 1024 * 1024 * 50; // 50MB
+}
