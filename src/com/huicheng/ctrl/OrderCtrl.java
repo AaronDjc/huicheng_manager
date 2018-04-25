ef6c370c7e5e529c8a8ac6c03655dc2a89285298
AaronDjc <aaron_djc@foxmail.com>
2018-04-25 16:18:31 +0800

    Add files via upload
    https://github.com/AaronDjc/huicheng_manager/upload

--- src/com/huicheng/ctrl/OrderCtrl.java
+++ src/com/huicheng/ctrl/OrderCtrl.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.huicheng.service.OrderService;
 
@@ -70,5 +72,12 @@ public class OrderCtrl {
 	public String editOrderNoData(HttpServletRequest req, HttpServletResponse res){
 		String result = orderService.editOrderNoData(req);
 		return result;
+	}
+	
+	@RequestMapping(value="upload",method=RequestMethod.POST,produces="text/html;charset=utf-8")
+	@ResponseBody
+	public String uploadExcelFile(@RequestParam(value="upfile")MultipartFile file){
+		String result = orderService.uploadExcelFile(file);
+        return result;  
 	}
 }
